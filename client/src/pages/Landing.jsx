5732feeecf06a3f76cdd280999adcc532c8ebefa
moise <mose.ns@hotmail.com>
2022-08-11 17:27:32 +0200

    Error page

--- client/src/pages/Landing.jsx
+++ client/src/pages/Landing.jsx
@@ -1,6 +1,7 @@
 import main from "../assets/images/main.svg";
 import Wrapper from "../assets/wrappers/LandingPage.js";
-import {Logo} from "../components"
+import { Logo } from "../components";
+import { Link } from "react-router-dom";
 
 
 const Landing = () => {
@@ -19,7 +20,7 @@ const Landing = () => {
             party. Tattooed man bun af biodiesel meditation asymmetrical
             shoreditch bitters hell of selfies gochujang squid williamsburg.{" "}
           </p>
-          <button className="btn btn-hero">Login/Register</button>
+          <Link to="/register" className="btn btn-hero">Login/Register</Link>
         </div>
         <img src={main} alt="job hunt" className="img main-img" />
       </div>
