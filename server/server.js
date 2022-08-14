887e511c89aac1d248cc8cd625871a4eb4fa5930
moise <mose.ns@hotmail.com>
2022-08-14 22:20:06 +0200

    Error handler middleware + installing dotenv

--- server/server.js
+++ server/server.js
@@ -1,12 +1,17 @@
 import express from "express";
-
+import notFoundMiddleware from "./middleware/not-found.js"
+import errorHandlerMidleware from "./middleware/error-handler.js"
 
 
 const app = express();
 
 app.get('/', (req, res) => {
+  //throw new Error("error")
   res.send('Welcome!')
 })
+
+app.use(notFoundMiddleware)
+app.use(errorHandlerMidleware)
 
 const port = process.env.PORT || 5000;
 
