1a00e979daf9e884a515ca7ad5f8975b1a815776
moise <mose.ns@hotmail.com>
2022-08-15 15:39:41 +0200

    modified server js

--- server/server.js
+++ server/server.js
@@ -22,7 +22,7 @@ app.get("/", (req, res) => {
 });
 
 app.use('/api/v1/auth', authRouter)
-app.use('.api/v1/jobs', jobsRouter)
+app.use('/api/v1/jobs', jobsRouter)
 
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
