c841276cf0b4ab55563f92d3de9c723abee9fdf1
moise <mose.ns@hotmail.com>
2022-08-17 08:51:20 +0200

    createJWT - Custom Instance Method

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -17,6 +17,7 @@ const register = async (req, res) => {
   }
 
   const user = await User.create({ name, email, password })
+  user.createJWT()
   res.status(StatusCodes.CREATED).json({ user })
 }
 
