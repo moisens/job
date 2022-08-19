c02289ad38829bc19b0dcba0161fcb42dd491c91
moise <mose.ns@hotmail.com>
2022-08-19 15:58:33 +0200

    Login user complete

--- client/src/context/reducer.jsx
+++ client/src/context/reducer.jsx
@@ -29,7 +29,7 @@ const reducer = (state, action) => {
   }
 
   if (action.type === REGISTER_USER_BEGIN) {
-    return { ...state, isLoading: true }
+    return { ...state, isLoading: true };
   }
 
   if (action.type === REGISTER_USER_SUCCESS) {
@@ -42,9 +42,8 @@ const reducer = (state, action) => {
       isLoading: false,
       showAlert: true,
       alertType: "success",
-      alertText: "User created! Redirecting..."
-
-    }
+      alertText: "User created! Redirecting...",
+    };
   }
 
   if (action.type === REGISTER_USER_ERROR) {
@@ -54,7 +53,38 @@ const reducer = (state, action) => {
       showAlert: true,
       alertType: "danger",
       alertText: action.payload.msg,
-    }
+    };
+  }
+
+  if (action.type === LOGIN_USER_BEGIN) {
+    return {
+      ...state,
+      isLoading: true,
+    };
+  }
+
+  if (action.type === LOGIN_USER_SUCCESS) {
+    return {
+      ...state,
+      isLoading: false,
+      user: action.payload.user,
+      token: action.payload.token,
+      userLocation: action.payload.location,
+      jobLocation: action.payload.location,
+      showAlert: true,
+      alertType: "success",
+      alertText: "Successfully logged in! Redirecting...",
+    };
+  }
+
+  if (action.type === LOGIN_USER_ERROR) {
+    return {
+      ...state,
+      isLoading: false,
+      showAlert: true,
+      alertType: "danger",
+      alertText: action.payload.msg,
+    };
   }
 
   throw new Error(`no such action: ${action.type}`);
