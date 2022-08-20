54c5a03db6dddbf43a393baa90e61fe98a2dec5d
moise <mose.ns@hotmail.com>
2022-08-20 19:54:59 +0200

    toggleSidebar functionality

--- client/src/context/reducer.jsx
+++ client/src/context/reducer.jsx
@@ -10,6 +10,8 @@ import {
   SETUP_USER_BEGIN,
   SETUP_USER_SUCCESS,
   SETUP_USER_ERROR,
+  TOGGLE_SIDEBAR,
+
 } from "./actions";
 
 const reducer = (state, action) => {
@@ -120,6 +122,13 @@ const reducer = (state, action) => {
       alertType: "danger",
       alertText: action.payload.msg,
     };
+  }
+  
+  if (action.type === TOGGLE_SIDEBAR) {
+    return {
+      ...state,
+      showSidebar: !state.showSidebar,
+    }
   }
 
   throw new Error(`no such action: ${action.type}`);
