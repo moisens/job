7b085651c341cb61a202414b1066f78d224aa173
moise <mose.ns@hotmail.com>
2022-08-20 17:08:48 +0200

    Navbar structure

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,12 +1,39 @@
 import Wrapper from "../assets/wrappers/Navbar";
-import { FaHome } from "react-icons/fa"
+import { FaAlignLeft, FaUserCircle, FaCaretDown } from "react-icons/fa";
+import { useState } from "react";
+import useAppContext from "../hooks/useAppContext";
+import Logo from "./Logo";
+
+
 
 
 const Navbar = () => {
   return (
     <Wrapper>
-      <h4>navbar</h4>
-      <FaHome />
+      <div className="nav-center">
+        <button 
+          className="toggle-btn"
+          onClick={() => console.log("toggle sidebar")}
+        >
+          <FaAlignLeft />
+        </button>
+        <div>
+          <Logo />
+          <h3 className="logo-text">dashboard</h3>
+        </div>
+        <div className="btn-container">
+          <button className="btn" onClick={() => console.log("show log out")}>
+            <FaUserCircle />
+            Brian
+            <FaCaretDown />
+          </button>
+          <div className="dropdown show-dropdown">
+            <button 
+              onClick={() => console.log("logout user")} 
+              className="dropdown-btn">logout</button>
+          </div>
+        </div>
+      </div>
     </Wrapper>
   )
 }
