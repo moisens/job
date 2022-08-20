afd95d227868ef578f7332acae990ecd296e5a72
moise <mose.ns@hotmail.com>
2022-08-20 16:20:02 +0200

    added react-icon library

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,9 +1,12 @@
 import Wrapper from "../assets/wrappers/Navbar";
+import { FaHome } from "react-icons/fa"
+
 
 const Navbar = () => {
   return (
     <Wrapper>
       <h4>navbar</h4>
+      <FaHome />
     </Wrapper>
   )
 }
