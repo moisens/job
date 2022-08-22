ee2c967d454951812828e586d29f1372ab31c57c
moise <mose.ns@hotmail.com>
2022-08-22 10:21:04 +0200

    auth complete

--- server/middleware/authenticate.js
+++ server/middleware/authenticate.js
@@ -1,12 +1,22 @@
+import jwt from "jsonwebtoken";
 import { UnauthenticatedError } from "../errors/index.js";
+
 
 const auth = async (req, res, next) => {
   const authHeader = req.headers.authorization;
-  if (!authHeader) {
-    throw new UnauthenticatedError('Authentication invalid!')
+  if (!authHeader || !authHeader.startsWith('Bearer')) {
+    throw new UnauthenticatedError('Authentication invalid!');
   }
-  
-  next();
+
+  const token = authHeader.split(" ")[1];
+  try {
+    const payload = jwt.verify(token, process.env.JWT_SECRET);
+    //req.user = payload;
+    req.user = { userId: payload.userId };
+    next();
+  } catch (error) {
+    throw new UnauthenticatedError('Authentication invalid!');
+  }
 };
 
 export default auth;
