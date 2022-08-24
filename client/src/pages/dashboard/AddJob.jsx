567fdcd669dd5433871531c411544304f7fe99dc
moise <mose.ns@hotmail.com>
2022-08-24 19:35:32 +0200

    add job page - structure

--- client/src/pages/dashboard/AddJob.jsx
+++ client/src/pages/dashboard/AddJob.jsx
@@ -1,5 +1,70 @@
+import { FormRow, Alert } from "../../components";
+import useAppContext from "../../hooks/useAppContext";
+import Wrapper from "../../assets/wrappers/DashboardFormPage";
+
 const AddJob = () => {
-  return <h1>Add Job Page!</h1>;
+  const {
+    isEditing,
+    showAlert,
+    displayAlert,
+    position,
+    company,
+    jobLocation,
+    jobType,
+    jobTypeOptions,
+    status,
+    statusOptions,
+  } = useAppContext();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!position || !company || !jobLocation) {
+      displayAlert();
+      return;
+    }
+    console.log('create job');
+  }
+
+  const handleJobInput = (e) => {
+    const name = e.target.name;
+    const value = e.target.value;
+    console.log(`${name}:${value}`);
+  }
+
+  return <Wrapper>
+    <form className="form">
+      <h3>{isEditing ? "edit job": "add job"}</h3>
+      {showAlert && <Alert />}
+      <div className="form-center">
+        <FormRow 
+          type="text"
+          name="position"
+          value={position}
+          handleChange={handleJobInput}
+        />
+        <FormRow 
+          type="text"
+          name="company"
+          value={company}
+          handleChange={handleJobInput}
+        />
+        <FormRow 
+          type="text"
+          labelText="job location"
+          name="jobLocation"
+          value={jobLocation}
+          handleChange={handleJobInput}
+        />
+        <div className="btn-container">
+          <button
+            className="btn btn-block submit-btn"
+            type="submit"
+            onClick={handleSubmit}
+          >submit</button>
+        </div>
+      </div>
+    </form>
+  </Wrapper>;
 };
 
 export default AddJob;
