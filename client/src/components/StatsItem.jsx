7825b4b254910eea707fd9b23614fb583d93eb3d
moise <mose.ns@hotmail.com>
2022-08-31 03:57:53 +0200

    stats items functionality

--- client/src/components/StatsItem.jsx
+++ client/src/components/StatsItem.jsx
@@ -1,9 +1,15 @@
+import Wrapper from "../assets/wrappers/StatItem";
 
-
-const StatsItem = () => {
-  return <div>
-    <h1>Stats item</h1>
-  </div>;
+const StatsItem = ({ count, title, icon, color, bcg }) => {
+  return (
+    <Wrapper color={color} bcg={bcg}>
+      <header>
+        <span className="count">{count}</span>
+        <span className="icon">{icon}</span>
+      </header>
+      <h5 className="title">{title}</h5>
+    </Wrapper>
+  );
 };
 
 export default StatsItem;
